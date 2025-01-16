ed4cd3c916643bbaf1068a2fb6be035a8eca2d35
Sougata <64636760+Sougata1119@users.noreply.github.com>
2025-01-16 08:48:47 +0530

    gtag included

--- script/main.js
+++ script/main.js
@@ -1,3 +1,22 @@
+// Google Tag (gtag.js)
+(function() {
+  // Dynamically create the script tag for gtag.js
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-720BWJCGW9";
+  document.head.appendChild(gtagScript);
+
+  // Initialize dataLayer and gtag function
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  window.gtag = gtag;
+
+  // Set the initial gtag configuration
+  gtag('js', new Date());
+  gtag('config', 'G-720BWJCGW9');
+})();
+
+
 // trigger to play music in the background with sweetalert
 window.addEventListener("load", () => {
   Swal.fire({
